342bfcd74211b3344d2bdbf6bf5f0383649d2436
mozix5 <mosinmd827@gmail.com>
2024-04-12 01:01:10 +0530

    added smooth scrolling

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import SmoothScrolling from "./components/SmoothScrolling.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <SmoothScrolling>
     <App />
-)
+  </SmoothScrolling>
+);
